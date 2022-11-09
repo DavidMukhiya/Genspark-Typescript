4aa536f859924732e79ef06d49cebc257014bed0
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2022-11-09 11:42:54 -0800

    learning array of object type using typescript

---
+++ structural-types/array-of-object-types/dist/array-of-object.js
@@ -0,0 +1,23 @@
+"use strict";
+const reviews = [
+    {
+        name: "Sheia",
+        stars: 5,
+        loyaltyUser: true,
+        date: "01-04-2021",
+    },
+    {
+        name: "Andrzej",
+        stars: 3,
+        loyaltyUser: false,
+        date: "28-03-2021",
+    },
+    {
+        name: "Omar",
+        stars: 4,
+        loyaltyUser: true,
+        date: "27-03-2021",
+    },
+];
+console.log(reviews);
+//# sourceMappingURL=array-of-object.js.map
