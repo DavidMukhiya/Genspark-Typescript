432685c1c457f3ed71582eff6b155644f671bdb2
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2022-11-09 11:25:30 -0800

    learning array type in typescript

---
+++ structural-types/array-types/dist/array-types.js
@@ -0,0 +1,11 @@
+"use strict";
+const you = {
+    firstname: 'David',
+    lastname: 'Mukhiya',
+    username: 'Dave',
+    isReturning: true,
+    age: 25,
+    stayedAt: ['florida-home', 'LA-flat', 'tokyo-bungalow']
+};
+console.log(you);
+//# sourceMappingURL=array-types.js.map
---
+++ structural-types/array-types/src/array-types.ts
@@ -0,0 +1,17 @@
+const you: {
+    firstname:string
+    lastname:string
+    username:string
+    isReturning: boolean
+    age:number
+    stayedAt:string[]
+} = {
+    firstname: 'David',
+    lastname: 'Mukhiya',
+    username:'Dave',
+    isReturning: true,
+    age: 25,
+    stayedAt:['florida-home', 'LA-flat', 'tokyo-bungalow']
+    }
+
+console.log(you)
