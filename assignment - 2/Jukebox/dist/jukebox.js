4dfcb45a3fbb673c3ed0ccfe74c39068a0595b28
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2022-11-10 13:42:26 -0800

    organize the file

--- assignment - 2/Jukebox/dist/jukebox.js
+++ assignment - 2/Jukebox/dist/jukebox.js
@@ -1,9 +1,5 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const song_1 = __importDefault(require("./song"));
 class Jukebox {
     constructor(playlist) {
         this.playlist = [];
@@ -18,15 +14,5 @@ class Jukebox {
         console.log(this.playlist);
     }
 }
-let song1 = new song_1.default("Let it be", "Beatles");
-let song2 = new song_1.default("I want it that way", "Backstreet Boys");
-let song3 = new song_1.default("Wake me up when september ends", "Green Day");
-let songList1 = [];
-songList1.push(song1);
-songList1.push(song2);
-songList1.push(song3);
-let playlist1 = new Jukebox(songList1);
-let newSong = new song_1.default("You belong with me", "Taylor Swift");
-playlist1.addSong(newSong);
-playlist1.playRandomSong();
+exports.default = Jukebox;
 //# sourceMappingURL=jukebox.js.map
